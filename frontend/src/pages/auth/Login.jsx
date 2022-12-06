cff1921cde9d8936860e28404e6a867ae812a85a
Abhi Patel <mr.abhiofficial100@gmail.com>
2022-12-06 12:12:19 +0530

    [Add] :- Validation on Login Page
    Validation on Login page is now working properly

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,64 +1,112 @@
-import React, { useState } from "react";
-import { Button, TextField, Typography } from "@mui/material";
+import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
 import { apiAuth } from "../../services/models/authModel";
 import { toast } from "react-hot-toast";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const navigate = useNavigate();
 
-  const loginUser = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+    submit: null,
+  }; // form field value validation schema
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email("Must be a valid email")
+      .max(255)
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password should be of minimum 6 characters length")
+      .required("Password is required"),
+  });
+
+  const { errors, values, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: (values) => {
+        // setLoading(true);
+        loginUser(values.email, values.password);
+      },
+    });
+
+  const loginUser = (email, password) => {
     const body = {
       email: email,
       password: password,
     };
 
+    // console.log({ body });
+
     apiAuth.post(body, "signin").then((res) => {
       if (res.status === "200") {
         navigate(`/${res.message.userId}`);
-      } else {
+        toast.success("Login successful");
+      } else if (res.status === "400") {
         toast.error(res.message);
+      } else {
+        toast.error("Error");
       }
     });
   };
 
   return (
     <React.Fragment>
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+      <Typography variant="h4" component="h1" sx={ { mb: 2 } }>
         Login
       </Typography>
-      <TextField
-        label="email"
-        size="small"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        sx={{ mb: 2 }}
-        fullWidth
-      />
-      <TextField
-        label="password"
-        size="small"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        fullWidth
-      />
-      <Button
-        variant="contained"
-        sx={{ display: "block", mt: 2, mx: "auto" }}
-        onClick={loginUser}
+      <Box
+        component="form"
+        noValidate
+        onSubmit={ handleSubmit }
+        style={ {
+          width: "100%",
+        } }
       >
-        Login
-      </Button>
-      <Typography variant="h6" component="p" sx={{ my: 2 }}>
-        Don't have an account Then{"  "}
-        <Link to="/signup" style={{ color: "deepskyblue" }}>
-          Signup
-        </Link>
-      </Typography>
+        <TextField
+          label="email"
+          size="small"
+          type="email"
+          sx={ { mb: 2 } }
+          fullWidth
+          name="email"
+          onBlur={ handleBlur }
+          onChange={ handleChange }
+          value={ values.email || "" }
+          error={ Boolean(touched.email && errors.email) }
+          helperText={ touched.email && errors.email }
+        />
+        <TextField
+          label="password"
+          size="small"
+          type="password"
+          fullWidth
+          name="password"
+          onBlur={ handleBlur }
+          onChange={ handleChange }
+          value={ values.password || "" }
+          error={ Boolean(touched.password && errors.password) }
+          helperText={ touched.password && errors.password }
+        />
+        <Button
+          variant="contained"
+          sx={ { display: "block", mt: 2, mx: "auto" } }
+          type="submit"
+        >
+          Login
+        </Button>
+        <Typography variant="h6" component="p" sx={ { my: 2 } }>
+          Don't have an account ? Then{ "  " }
+          <Link to="/signup" style={ { color: "deepskyblue" } }>
+            Signup
+          </Link>
+        </Typography>
+      </Box>
     </React.Fragment>
   );
 };
