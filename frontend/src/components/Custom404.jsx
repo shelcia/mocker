e73c527dc63e661a13c46a1f16b3b9e3d3450845
Abhi Patel <mr.abhiofficial100@gmail.com>
2022-12-08 20:59:54 +0530

    [ADDED] 404 Not Found Page Added

---
+++ frontend/src/components/Custom404.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Box, Button, Container, Typography, CardContent } from '@mui/material';
+import Grid from '@mui/material/Grid';
+
+const Custom404 = () => {
+    return (
+        <>
+            <React.Fragment>
+                <CardContent>
+                    <Box
+                        sx={ {
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            flexDirection: 'column',
+                            p: 4
+                        } }
+                    >
+                        <Typography variant="h1" >
+                            404
+                        </Typography>
+                        <Typography variant="h6" sx={{mt:1}}>
+                            The page you’re looking for doesn’t exist.
+                        </Typography>
+                        <Button variant="contained" sx={{mt:3}} onClick={() =>window.history.back()} >Back</Button>
+                    </Box>
+                </CardContent>
+            </React.Fragment>
+        </>
+    )
+}
+
+export default Custom404
