aae5af467b84327f1a0440ae4d8617142852397a
Abhi Patel <mr.abhiofficial100@gmail.com>
2022-12-10 18:07:10 +0530

    Clean Up

--- frontend/src/components/Custom404.jsx
+++ frontend/src/components/Custom404.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Box, Button, Container, Typography, CardContent } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import { Box, Button, Typography, CardContent } from '@mui/material';
 
 const Custom404 = () => {
     return (
@@ -13,7 +12,7 @@ const Custom404 = () => {
                             justifyContent: 'center',
                             alignItems: 'center',
                             flexDirection: 'column',
-                            p: 4
+                            p: 5
                         } }
                     >
                         <Typography variant="h1" >
@@ -22,7 +21,7 @@ const Custom404 = () => {
                         <Typography variant="h6" sx={{mt:1}}>
                             The page you’re looking for doesn’t exist.
                         </Typography>
-                        <Button variant="contained" sx={{mt:3}} onClick={() =>window.history.back()} >Back</Button>
+                        <Button variant="contained" sx={{mt:3}} onClick={() => window.history.back()} >Back</Button>
                     </Box>
                 </CardContent>
             </React.Fragment>
